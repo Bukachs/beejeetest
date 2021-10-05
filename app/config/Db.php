32ebc51c424085ff9b07ac818b93bd09b736fcef
Bukachs <mail@bukach.com>
2021-10-05 21:59:48 +0300

    Add files via upload

--- app/config/Db.php
+++ app/config/Db.php
@@ -1,1 +1,15 @@
+<?php
 
+return [
+  'map' => [
+    'tasks' => [
+      'task_id',
+      'task_name',
+      'task_body',
+      'task_closed',
+      'user_name',
+      'user_email',
+      'task_date_edited'
+    ]
+  ]
+];
